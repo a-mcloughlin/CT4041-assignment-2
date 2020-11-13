9e4b20e4721720787ae10b34d761542573f49dc8
Aideen McLoughlin <aideen.mcloughlin@gmail.com>
2020-11-13 12:45:04 +0000

    Comments of whos doing what

--- implementation.py
+++ implementation.py
@@ -7,27 +7,34 @@ def main():
     visualise_tree(tree)
     test_tree(tree, testing_data)
 
+# Aideen 
 def split_data(data):
     return training, testing
 
+# Aideen 
 def read_csv_data(csv_path):
     return data
 
 def data_class_check(data):
     return result
 
+# Come back to 
 def build_tree(data):
     return tree
 
+# Aideen
 def entropy(leaf):
     return entropy
 
+# Louise
 def information_gain(leaf):
     return information_gain
 
+# Louise
 def visualise_tree(tree):
     # visualaise
    
+# Come back to 
 def test_tree(tree, testing_data):
     # test tree
     
