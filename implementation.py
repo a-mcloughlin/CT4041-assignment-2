d40c476288917a5b3b02ec133e48582e5ae644c8
Aideen McLoughlin <aideen.mcloughlin@gmail.com>
2020-11-13 15:12:09 +0000

    Fix division of data, and create entropy function

--- implementation.py
+++ implementation.py
@@ -1,9 +1,12 @@
 # Main file for the implementation of the C4.5 algorithm
 import pandas as pd
+import numpy as np
+from math import log2
 
 def main():
     data = pd.read_csv('beer.csv')
     train_data, train_target, test_data, test_target = split_data(data, (2/3))
+    # entropy(train_target)
     #tree = build_tree(train_data, train_target)
     #visualise_tree(tree)
     #test_tree(tree, test_data, test_target)
@@ -16,9 +19,9 @@ def split_data(data, ratio):
     
     data = data.drop(columns=['beer_id'])
     
-    train = data.iloc[:division_point, :]
-    test = data.iloc[division_point:, :]
-
+    train = data.sample(frac=ratio,random_state=5)
+    test = data.sample(frac=(1-ratio),random_state=5)
+    
     train_data = train.drop(columns=['style'])
     train_target = train['style'].values
     test_data = test.drop(columns=['style'])
@@ -38,8 +41,13 @@ def read_csv_data(csv_path):
 #     return tree
 
 # # Aideen
-# def entropy(leaf):
-#     return entropy
+def entropy(train_target):
+
+    ale = np.count_nonzero(train_target == 'ale')/ len(train_target)
+    lager = np.count_nonzero(train_target == 'lager')/ len(train_target)
+    stout = np.count_nonzero(train_target == 'stout')/ len(train_target)
+    entropy = -((ale)*log2(ale) + (lager)*log2(lager) + (stout)*log2(stout) )
+    return entropy
 
 # # Louise
 # def information_gain(leaf):
