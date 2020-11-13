ea85f198ddb14a6c83de2b1ae5b6be053b871d1c
Aideen McLoughlin <aideen.mcloughlin@gmail.com>
2020-11-13 17:30:07 +0000

    Split implementation of data_split into 3 functions

--- implementation.py
+++ implementation.py
@@ -6,16 +6,27 @@ from math import log2
 
 def main():
     data = pd.read_csv('beer.csv')
-    train_data, train_target, test_data, test_target = split_data(data, (2/3))
-    # entropy(train_target)
-    #need to create subset of data from train_target for the le_subset, lager_subset, stout_subset()
-    # gain = information_gain(train_target, [ale_subset, lager_subset, stout_subset])
+    train, test = split_data_training_testing(data, (2/3))
+    ale_subset, lager_subset, stout_subset = split_data_styles(train)
+    train_data, train_target, test_data, test_target = split_into_data_target(train, test)
+    #need to create subset of data from train_target for the ale_subset, lager_subset, stout_subset()
+    #gain = information_gain(train_target, [ale_subset, lager_subset, stout_subset])
     #tree = build_tree(train_data, train_target)
     #visualise_tree(tree)
     #test_tree(tree, test_data, test_target)
 
+
+# Aideen
+def split_data_styles(data):
+    grouped = data.groupby(data['style'])
+    ale_subset = grouped.get_group('ale')
+    lager_subset = grouped.get_group('lager')
+    stout_subset = grouped.get_group('stout')
+    
+    return ale_subset, lager_subset, stout_subset
+
 # Aideen 
-def split_data(data, ratio):
+def split_data_training_testing(data, ratio):
 
     division_point = round(len(data)*ratio)
     headers = data.iloc[0]
@@ -24,7 +35,9 @@ def split_data(data, ratio):
     
     train = data.sample(frac=ratio,random_state=5)
     test = data.sample(frac=(1-ratio),random_state=5)
-    
+    return train, test
+
+def split_into_data_target(train, test):
     train_data = train.drop(columns=['style'])
     train_target = train['style'].values
     test_data = test.drop(columns=['style'])
